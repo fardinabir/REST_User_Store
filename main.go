7b0cadc8123b3902f2734512d5258b2b56f8e962
Kazi Fardin Islam Abir <fardinabir@gmail.com>
2022-10-30 18:39:04 +0600

    Extened tasks

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 
@@ -40,22 +41,44 @@ type User struct {
 	Phone     string `json:"phone"`
 }
 
+type UserTag struct {
+	gorm.Model
+	UserId     uint   `json:userId`
+	Name       string `json:"fullName"`
+	Tag        string `json:"tag"`
+	ExpiryTime string `json:"expiryTime"`
+}
+
+type GetResponse struct {
+	ID    uint   `json:"id"`
+	Name  string `json:"fullName"`
+	Phone string `json:"phone"`
+}
+
+type PostResponse struct {
+	ID uint `json:"id"`
+}
+
+type TagPostReq struct {
+	Tags   []string `json:"tags"`
+	Expiry uint     `json:"expiry"`
+}
+
 func intialMigration() {
 	DB, err = gorm.Open(mysql.Open(DSN), &gorm.Config{})
 	if err != nil {
 		fmt.Println(err.Error())
 		panic("cannot connect DB")
 	}
-	DB.AutoMigrate(&User{})
+	DB.AutoMigrate(&User{}, &UserTag{})
 }
 
 func returnAllUsers(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: returnAllArticles")
 	w.Header().Set("Content-Type", "application/json")
-	var userss []User
-	DB.Find(&userss)
-	json.NewEncoder(w).Encode(userss)
-
+	var users []User
+	DB.Find(&users)
+	json.NewEncoder(w).Encode(users)
 }
 
 func createNewUser(w http.ResponseWriter, r *http.Request) {
@@ -65,7 +88,9 @@ func createNewUser(w http.ResponseWriter, r *http.Request) {
 	json.NewDecoder(r.Body).Decode(&user)
 	DB.Create(&user)
 	fmt.Println(user)
-	json.NewEncoder(w).Encode(user)
+	var res PostResponse
+	res.ID = user.ID
+	json.NewEncoder(w).Encode(res)
 }
 
 func updateUser(w http.ResponseWriter, r *http.Request) {
@@ -73,6 +98,8 @@ func updateUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	var user User
+	json.NewDecoder(r.Body).Decode(&user)
+
 	DB.First(&user, params["id"])
 	json.NewDecoder(r.Body).Decode(&user)
 	DB.Save(&user)
@@ -80,13 +107,61 @@ func updateUser(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(user)
 }
 
+func updateUserTag(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: updateUserTag")
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	var req TagPostReq
+	json.NewDecoder(r.Body).Decode(&req) // Decoding tags & expiry
+
+	var user User
+	result := DB.First(&user, params["id"])
+	fmt.Println(result)
+	if result.Error != nil {
+		fmt.Println(result.Error)
+		w.WriteHeader(404)
+		return
+		//panic("Cannot Find User")
+	}
+
+	// Createing multiple rows for each user with single tag
+	for ind, tag := range req.Tags {
+		var uTag []UserTag
+		uTag.UserId, uTag.Name = user.ID, user.FirstName+" "+user.LastName
+		uTag.Tag = tag
+
+		// Setting an Expiry time by adding the duration with reqest submit time, later it will be compared
+		uTag.ExpiryTime = time.Now().Add(time.Duration(req.Expiry) * time.Millisecond)
+		fmt.Println(time.Now())
+		fmt.Println(time.Now().Add(time.Duration(req.Expiry) * time.Millisecond))
+		DB.Create(&uTag)
+		uTag.ID = user.ID
+		DB.Save(&uTag)
+	}
+
+}
+
 func getUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: getUser")
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	var user User
-	DB.First(&user, params["id"])
-	json.NewEncoder(w).Encode(user)
+	result := DB.First(&user, params["id"])
+	fmt.Println(result)
+	if result.Error != nil {
+		fmt.Println(result.Error)
+		w.WriteHeader(404)
+		return
+		//panic("Cannot Find User")
+	}
+	var res GetResponse
+	res.ID, res.Name, res.Phone = user.ID, user.FirstName+" "+user.LastName, user.Phone
+	json.NewEncoder(w).Encode(res)
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request, i int) {
+	panic("unimplemented")
 }
 
 func deleteUser(w http.ResponseWriter, r *http.Request) {
@@ -102,11 +177,16 @@ func handleRequests() {
 	myRouter := mux.NewRouter()
 
 	myRouter.HandleFunc("/users", createNewUser).Methods("POST")
-	myRouter.HandleFunc("/users", updateUser).Methods("PUT")
+	myRouter.HandleFunc("/users/{id}", getUser).Methods("GET")
 
 	myRouter.HandleFunc("/users", returnAllUsers).Methods("GET")
-	myRouter.HandleFunc("/users/{id}", getUser).Methods("GET")
 	myRouter.HandleFunc("/users/{id}", deleteUser).Methods("DELETE")
+	myRouter.HandleFunc("/users", updateUser).Methods("PUT")
+	http.Handle("/", myRouter)
+
+	// Extended Tasks
+	myRouter.HandleFunc("/users/{id}/tags", updateUserTag).Methods("POST")
+
 	//myRouter.HandleFunc("/article/{id}", returnSingleArticle)
 
 	http.ListenAndServe(":8000", myRouter)
